fe46668bbc6898447a9448fb00189073f312ddfc
Pradip Dhara <pradipdhara48@gmail.com>
2026-08-31 23:44:21 +0530

    Render all sections on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,34 @@
-import { SiteNav } from '@/components/site-nav'
-import { Hero } from '@/components/sections/hero'
-import { About } from '@/components/sections/about'
-import { Details } from '@/components/sections/details'
-import { Gallery } from '@/components/sections/gallery'
-import { RsvpFooter } from '@/components/sections/rsvp-footer'
-import { FloatingWhatsApp } from '@/components/floating-whatsapp'
+import { Hero } from '@/components/sections/hero';
+import { About } from '@/components/sections/about';
+import { Details } from '@/components/sections/details';
+import { Gallery } from '@/components/sections/gallery';
+import { RsvpFooter } from '@/components/sections/rsvp-footer';
+import { SiteNav } from '@/components/site-nav';
+import { FloatingWhatsapp } from '@/components/floating-whatsapp';
 
 export default function Home() {
   return (
-    <main className="relative">
+    <main className="min-h-screen bg-[#faf8f5] text-[#2c2420] selection:bg-[#8b4513] selection:text-white">
+      {/* Navigation Bar */}
       <SiteNav />
+
+      {/* Hero / Cover Section */}
       <Hero />
+
+      {/* About the Couple */}
       <About />
+
+      {/* Wedding Events & Location Details */}
       <Details />
+
+      {/* Photo Gallery / Glimpse of Us */}
       <Gallery />
+
+      {/* RSVP Form and Footer */}
       <RsvpFooter />
-      <FloatingWhatsApp />
+
+      {/* Floating WhatsApp Quick Action */}
+      <FloatingWhatsapp />
     </main>
-  )
+  );
 }
